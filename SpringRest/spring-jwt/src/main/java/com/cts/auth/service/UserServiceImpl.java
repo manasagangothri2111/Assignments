a31d70d6bee8fc9d73159209eca6deb52c1df34c
manasa <manasa.tirunagari@cognizant.com>
2024-06-17 03:33:10 +0000

    adding spring jwt code

---
+++ SpringRest/spring-jwt/src/main/java/com/cts/auth/service/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.cts.auth.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.cts.auth.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found !!!"));
+    }
+}
